c15d8d80c629c07a563f7d376b19f16e782313c7
Liberbell <liberbell@gmail.com>
2021-10-15 08:29:47 +0900

    main2

--- pj3/main.go
+++ pj3/main.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	var i int = 100
 	fmt.Println(i)
+	fmt.Println(reflect.TypeOf(i))
 
 	var i2 int64 = 200
+	fmt.Println(reflect.TypeOf(i2))
 	fmt.Println(i + 50)
 
-	fmt.Println(i + i2)
+	// fmt.Println(i + i2)
 
 }
