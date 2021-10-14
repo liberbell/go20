38c73e6180d0d27cf95aed7c6c9230317a1a4573
Liberbell <liberbell@gmail.com>
2021-10-15 08:33:42 +0900

    main5

--- pj3/main.go
+++ pj3/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	var i int = 100
-	fmt.Println(i)
-	fmt.Println(reflect.TypeOf(i))
+	// var i int = 100
+	// fmt.Println(i)
+	// fmt.Println(reflect.TypeOf(i))
 
-	var i2 int64 = 200
-	fmt.Println(reflect.TypeOf(i2))
-	fmt.Printf("Type of i2: %T\n", i2)
-	fmt.Printf("Convert type of i2: %T\n", int32(i2))
-	fmt.Println(i + 50)
+	// var i2 int64 = 200
+	// fmt.Println(reflect.TypeOf(i2))
+	// fmt.Printf("Type of i2: %T\n", i2)
+	// fmt.Printf("Convert type of i2: %T\n", int32(i2))
+	// fmt.Println(i + 50)
 
 	// fmt.Println(i + i2)
+	var fl64 float64 = 2.4
+	fmt.Println(fl64)
 
 }
