050d346b173eced31edf8130d4084f4422c40a44
Liberbell <liberbell@gmail.com>
2021-10-16 14:27:48 +0900

    main3

--- pj7/main.go
+++ pj7/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var arr1 [1]int
+	var arr1 [3]int
 	fmt.Println(arr1)
 	fmt.Printf("Type : %T\n", arr1)
+
+	var arr2 [3]string = [3]string{"today", "is", "fine."}
+	fmt.Println(arr2)
+	fmt.Printf("Type : %T\n", arr2)
 }
