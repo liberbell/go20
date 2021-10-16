5004bb314c85c066461a565664160a5013f6cd1d
Liberbell <liberbell@gmail.com>
2021-10-16 14:30:02 +0900

    main4

--- pj7/main.go
+++ pj7/main.go
@@ -10,4 +10,8 @@ func main() {
 	var arr2 [3]string = [3]string{"today", "is", "fine."}
 	fmt.Println(arr2)
 	fmt.Printf("Type : %T\n", arr2)
+
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
+	fmt.Printf("Type : %T\n", ar3)
 }
