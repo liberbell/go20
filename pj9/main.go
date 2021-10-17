0d6c7bd35058b2ce4edadc9870bd79ddac1cc2be
Liberbell <liberbell@gmail.com>
2021-10-17 10:37:44 +0900

    main6

--- pj9/main.go
+++ pj9/main.go
@@ -1,21 +1,8 @@
 package main
 
-import "fmt"
-
-func main() {
-	// var i int = 1
-	// fmt.Printf("Type of i : %T\n", i)
-	// fl64 := float64(i)
-	// fmt.Println(fl64)
-	// fmt.Printf("Type of fl64: %T\n", fl64)
-
-	// i2 := int(fl64)
-	// fmt.Println(i2)
-	// fmt.Printf("Type of i2: %T\n", i2)
-
-	// fl := 10.3
-	// fmt.Printf("Type of fl : %T\n", fl)
-	// fmt.Println(fl)
+import (
+	"fmt"
+	"strconv"
 
 	// i3 := int(fl)
 	// fmt.Printf("Type of i3 : %T\n", i3)
@@ -23,4 +10,7 @@ func main() {
 
 	var s string = "100"
 	fmt.Printf("Type of s : %T\n", s)
+	i, _ := strconv.Atoi(s)
+	fmt.Println(i)
+	fmt.Printf("Type of conv i : %T\n", i)
 }
