e8e0e255eb0a6dae14ef74c6bdbab619cda49f1a
Liberbell <liberbell@gmail.com>
2021-10-17 10:36:42 +0900

    main6

--- pj9/main.go
+++ pj9/main.go
@@ -3,21 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var i int = 1
-	fmt.Printf("Type of i : %T\n", i)
-	fl64 := float64(i)
-	fmt.Println(fl64)
-	fmt.Printf("Type of fl64: %T\n", fl64)
+	// var i int = 1
+	// fmt.Printf("Type of i : %T\n", i)
+	// fl64 := float64(i)
+	// fmt.Println(fl64)
+	// fmt.Printf("Type of fl64: %T\n", fl64)
 
-	i2 := int(fl64)
-	fmt.Println(i2)
-	fmt.Printf("Type of i2: %T\n", i2)
+	// i2 := int(fl64)
+	// fmt.Println(i2)
+	// fmt.Printf("Type of i2: %T\n", i2)
 
-	fl := 10.3
-	fmt.Printf("Type of fl : %T\n", fl)
-	fmt.Println(fl)
+	// fl := 10.3
+	// fmt.Printf("Type of fl : %T\n", fl)
+	// fmt.Println(fl)
 
-	i3 := int(fl)
-	fmt.Printf("Type of i3 : %T\n", i3)
-	fmt.Println(i3)
+	// i3 := int(fl)
+	// fmt.Printf("Type of i3 : %T\n", i3)
+	// fmt.Println(i3)
+
+	var s string = "100"
+	fmt.Printf("Type of s : %T\n", s)
 }
