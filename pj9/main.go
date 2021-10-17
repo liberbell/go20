0a61c45063aca2068116d2d6a90fc596f104cdbd
Liberbell <liberbell@gmail.com>
2021-10-17 10:32:14 +0900

    main3

--- pj9/main.go
+++ pj9/main.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	var i int = 1
+	fmt.Printf("Type of i : %T\n", i)
 	fl64 := float64(i)
 	fmt.Println(fl64)
-	fmt.Printf("Type : %T\n", fl64)
+	fmt.Printf("Type of fl64: %T\n", fl64)
 }
