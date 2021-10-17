c31994b4b61923b68b2998fadad5f943d6dbe132
Liberbell <liberbell@gmail.com>
2021-10-17 10:51:15 +0900

    main3

--- pj10/main.go
+++ pj10/main.go
@@ -4,6 +4,17 @@ import "fmt"
 
 const Pi = 3.14
 
+const (
+	URL      = "http://www.yahoo.co.jp"
+	Sitename = "Yahoo"
+)
+
 func main() {
 	fmt.Println(Pi)
+
+	Pi := 3
+	fmt.Println(Pi)
+
+	fmt.Println(URL, Sitename)
+
 }
