494d9cbaa3e101b00069f35703a7287c4b0f64ff
Liberbell <liberbell@gmail.com>
2021-10-20 09:14:25 +0900

    main3

--- pj20/main.go
+++ pj20/main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	i := 0
-	for {
-		fmt.Printf("Loop %d\n", i)
-		time.Sleep(time.Millisecond * 10)
-		i++
-		if i == 100 {
-			break
-		}
+	// i := 0
+	// for {
+	// 	fmt.Printf("Loop %d\n", i)
+	// 	time.Sleep(time.Millisecond * 10)
+	// 	i++
+	// 	if i == 100 {
+	// 		break
+	// 	}
+	// }
+
+	point := 0
+	for point < 10 {
+		fmt.Println(point)
+		point++
 	}
 }
