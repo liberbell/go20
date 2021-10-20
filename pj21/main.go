cc3c6f9bf3ff665e2715b0cfdec12d1380d97c0b
Liberbell <liberbell@gmail.com>
2021-10-21 08:58:10 +0900

    main4

--- pj21/main.go
+++ pj21/main.go
@@ -13,12 +13,20 @@ func main() {
 	// 	fmt.Println("some number")
 	// }
 
-	switch n := 2; n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
-	default:
-		fmt.Println("some number")
+	// switch n := 2; n {
+	// case 1, 2:
+	// 	fmt.Println("1 or 2")
+	// case 3, 4:
+	// 	fmt.Println("3 or 4")
+	// default:
+	// 	fmt.Println("some number")
+	// }
+
+	n := 6
+	switch {
+	case n > 0 && n < 4:
+		fmt.Println("less than 4")
+	case n > 3 && n < 7:
+		fmt.Println("from 4 to 7")
 	}
 }
