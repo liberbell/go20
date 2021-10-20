d61bd653aa09edb6dd3d6257b856ef6a50a61ba4
Liberbell <liberbell@gmail.com>
2021-10-21 08:52:06 +0900

    main1

---
+++ pj21/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 1
+	switch n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("some number")
+	}
+}
