c87b031dc50e8a29069926fb2632fa23e690640c
Liberbell <liberbell@gmail.com>
2021-10-22 08:57:58 +0900

    main4

--- pj23/main.go
+++ pj23/main.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 func main() {
-Loop:
-	for {
-		for {
-			for {
-				fmt.Println("START")
-				break Loop
-			}
-			fmt.Println("Don`t operate 1")
+	// Loop:
+	// 	for {
+	// 		for {
+	// 			for {
+	// 				fmt.Println("START")
+	// 				break Loop
+	// 			}
+	// 			fmt.Println("Don`t operate 1")
+	// 		}
+	// 		fmt.Println("Don`t operate 2")
+	// 	}
+	// 	fmt.Println("END")
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Println(i, j)
 		}
-		fmt.Println("Don`t operate 2")
 	}
-	fmt.Println("END")
 }
