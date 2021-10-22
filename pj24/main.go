aa833512c99342355bd20f48a3bf5e5854382b14
Liberbell <liberbell@gmail.com>
2021-10-22 09:08:59 +0900

    main4

--- pj24/main.go
+++ pj24/main.go
@@ -6,6 +6,13 @@ func TestDefer() {
 	defer fmt.Println("END")
 	fmt.Println("START")
 }
+
+func RunDefer() {
+	defer fmt.Println("1")
+	defer fmt.Println("2")
+	defer fmt.Println("3")
+}
+
 func main() {
 	TestDefer()
 
