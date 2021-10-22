ff770e7e42d075df21d3b5d63cbbbbdac6eb70fa
Liberbell <liberbell@gmail.com>
2021-10-22 09:03:04 +0900

    main6

--- pj23/main.go
+++ pj23/main.go
@@ -16,10 +16,11 @@ func main() {
 	// 	}
 	// 	fmt.Println("END")
 
+Loop:
 	for i := 0; i < 3; i++ {
 		for j := 1; j < 3; j++ {
 			if j > 1 {
-				continue
+				continue Loop
 			}
 			fmt.Println(i, j, i*j)
 		}
