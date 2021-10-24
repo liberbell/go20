93c6bb21f79ecca9d60890a751577c4b2546b877
Liberbell <liberbell@gmail.com>
2021-10-24 12:19:57 +0900

    main4

--- pj30/main.go
+++ pj30/main.go
@@ -3,18 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	sl := []int{100, 200}
-	sl2 := sl
+	// sl := []int{100, 200}
+	// sl2 := sl
+
+	// fmt.Println(sl)
+	// fmt.Println(sl2)
+	// sl2[0] = 500
+	// fmt.Println(sl)
+	// fmt.Println(sl2)
+
+	// var i int = 10
+	// i2 := i
+	// i2 = 100
+	// fmt.Println(i)
+	// fmt.Println(i2)
+
+	sl := []int{1, 2, 3, 4, 5}
+	sl2 := make([]int, 5, 10)
+	n := copy(sl2, sl)
 
 	fmt.Println(sl)
 	fmt.Println(sl2)
-	sl2[0] = 500
-	fmt.Println(sl)
-	fmt.Println(sl2)
-
-	var i int = 10
-	i2 := i
-	i2 = 100
-	fmt.Println(i)
-	fmt.Println(i2)
+	fmt.Println(n)
 }
