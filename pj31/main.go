3ca4640fe8ebee2e2f5f69ac4810c36dc3edfbff
Liberbell <liberbell@gmail.com>
2021-10-24 12:25:04 +0900

    main5

--- pj31/main.go
+++ pj31/main.go
@@ -6,7 +6,10 @@ func main() {
 	sl := []string{"A", "B", "C"}
 	fmt.Println(sl)
 
-	for i, v := range sl {
-		fmt.Println(i, v)
+	// for i, v := range sl {
+	// 	fmt.Println(i, v)
+	// }
+	for i := 0; i < len(sl); i++ {
+		fmt.Println(sl[i])
 	}
 }
