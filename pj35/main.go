8ea39656835045c65ff33fe552d8fd6defb1065a
Liberbell <liberbell@gmail.com>
2021-10-25 09:13:36 +0900

    main4

--- pj35/main.go
+++ pj35/main.go
@@ -6,9 +6,9 @@ func main() {
 	// var ch2 <-chan int
 	// var ch3 chan<- int
 
-	ch1 = make(chan, int)
-	ch2 := make(chan, int)
-	
+	ch1 = make(chan int)
+	ch2 := make(chan int)
+
 	fmt.println(cap(ch1))
 	fmt.println(cap(ch2))
 }
