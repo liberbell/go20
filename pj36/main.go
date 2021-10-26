7402c773588570584098f8c256c46a192f35bd8a
Liberbell <liberbell@gmail.com>
2021-10-26 09:13:04 +0900

    main2

--- pj36/main.go
+++ pj36/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+func reciever(c chan int) {
+	for {
+		i := <-c
+		fmt.Println(i)
+	}
+
+}
+
 func main() {
 	ch1 := make(chan int)
-	fmt.Println(<-ch1)
+	// fmt.Println(<-ch1)
+
 }
