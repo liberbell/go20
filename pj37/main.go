671fa1cc954fcf2f9ee10994eb8670def6500dfc
Liberbell <liberbell@gmail.com>
2021-10-27 08:41:54 +0900

    main3

--- pj37/main.go
+++ pj37/main.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
 	ch1 := make(chan int, 2)
-	// close(ch1)
+
+	ch1 <- 1
+	close(ch1)
 	// ch1 <- 2
 
 	// i := <-ch1
 	// fmt.Println(i)
-	ch1 <- 1
+	// ch1 <- 1
 
 	i, ok := <-ch1
 	fmt.Println(i, ok)
