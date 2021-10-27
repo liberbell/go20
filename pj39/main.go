8a4ece35df34a1ce28937530aa54fff9eed1a5ff
Liberbell <liberbell@gmail.com>
2021-10-28 08:56:43 +0900

    main2

--- pj39/main.go
+++ pj39/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+func Double(i int) {
+	i = i * 2
+}
+
 func main() {
 	var n int = 100
 	fmt.Println(n)
+	fmt.Println(&n)
+
+	Double(n)
+	fmt.Println(n)
+	fmt.Println(&n)
 }
