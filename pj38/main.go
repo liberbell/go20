98bbca94ea79eb7cc0c4abb80c654271a46fd5b4
Liberbell <liberbell@gmail.com>
2021-10-27 09:13:35 +0900

    main7

--- pj38/main.go
+++ pj38/main.go
@@ -16,10 +16,10 @@ func main() {
 	ch1 := make(chan int, 2)
 	ch2 := make(chan string, 2)
 
-	ch2 <- "A"
-	ch1 <- 1
-	ch2 <- "B"
-	ch1 <- 2
+	// ch2 <- "A"
+	// ch1 <- 1
+	// ch2 <- "B"
+	// ch1 <- 2
 
 	// v1 := <-ch1
 	// v2 := <-ch2
@@ -30,5 +30,7 @@ func main() {
 		fmt.Println(v1 + 1000)
 	case v2 := <-ch2:
 		fmt.Println(v2 + "!")
+	default:
+		fmt.Println("unknown")
 	}
 }
