fb585da5a8cc54f7d5665a3b5c35f51ca20f3733
Liberbell <liberbell@gmail.com>
2021-10-29 08:40:01 +0900

    main3

--- pj40/main.go
+++ pj40/main.go
@@ -13,4 +13,8 @@ func main() {
 	user1.Age = 10
 	fmt.Println("Name: ", user1.Name)
 	fmt.Println("Age: ", user1.Age)
+
+	user2 := User{}
+	fmt.Println("Name: ", user2.Name)
+	fmt.Println("Age: ", user2.Age)
 }
