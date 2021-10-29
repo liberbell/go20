7f1325ba13019626f204944cf5baa2eb6b455a4d
Liberbell <liberbell@gmail.com>
2021-10-29 09:04:16 +0900

    main2

--- pj41/main.go
+++ pj41/main.go
@@ -11,7 +11,14 @@ func (u User) SayName() {
 	fmt.Println(u.Name)
 }
 
+func (u User) SetName(name string) {
+	u.Name = name
+}
+
 func main() {
 	user1 := User{Name: "Bob"}
+	user1.SayName()
+
+	user1.SetName("Eric")
 	user1.SayName()
 }
