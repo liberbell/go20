56893c18319dd46d10b98ee862f82135b0f6c88e
Liberbell <liberbell@gmail.com>
2021-10-30 15:59:41 +0900

    main2

--- pj42/main.go
+++ pj42/main.go
@@ -11,10 +11,17 @@ type User struct {
 	Age  int
 }
 
+func (u *User) SetName() {
+	u.Name = "Anybody"
+}
+
 func main() {
 	t := T{User: User{Name: "Bob", Age: 20}}
 	fmt.Println(t)
 
 	fmt.Println(t.User)
 	fmt.Println(t.User.Name)
+
+	t.User.SetName()
+	fmt.Println(t)
 }
