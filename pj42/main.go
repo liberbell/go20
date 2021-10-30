de64a684e338e169aff9c59c2f4898db342afdd0
Liberbell <liberbell@gmail.com>
2021-10-30 16:01:47 +0900

    main3

--- pj42/main.go
+++ pj42/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type T struct {
 	User User
 }
@@ -11,17 +9,22 @@ type User struct {
 	Age  int
 }
 
+func NewUser(name string, age int) *User {
+	return &User{Name: name, Age: age}
+}
+
 func (u *User) SetName() {
 	u.Name = "Anybody"
 }
 
 func main() {
-	t := T{User: User{Name: "Bob", Age: 20}}
-	fmt.Println(t)
+	// t := T{User: User{Name: "Bob", Age: 20}}
+	// fmt.Println(t)
 
-	fmt.Println(t.User)
-	fmt.Println(t.User.Name)
+	// fmt.Println(t.User)
+	// fmt.Println(t.User.Name)
 
-	t.User.SetName()
-	fmt.Println(t)
+	// t.User.SetName()
+	// fmt.Println(t)
+
 }
