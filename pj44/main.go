a722216ec430be43051f3ab4b5847bcdfd31564f
Liberbell <liberbell@gmail.com>
2021-10-30 16:25:10 +0900

    main2

--- pj44/main.go
+++ pj44/main.go
@@ -2,14 +2,14 @@ package main
 
 type User struct {
 	Name string
-	Age int
+	Age  int
 }
 
 func main() {
 	m := map[int]User{
-		1: {Name: "Bob", Age: 10},
-		2: {Name: "Eric", Age: 20},
+		1: {Name: "Poul", Age: 10},
+		2: {Name: "Jhon", Age: 20},
 		3: {Name: "George", Age: 30},
-		4: {Name: "Ringo", Age: 40}
+		4: {Name: "Ringo", Age: 40},
 	}
 }
