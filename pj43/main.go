fb9361b95e134d9f66715e8fee88820ca87f9654
Liberbell <liberbell@gmail.com>
2021-10-30 16:10:40 +0900

    main3

--- pj43/main.go
+++ pj43/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type User struct {
 	Name string
 	Age  int
@@ -12,4 +14,10 @@ func main() {
 	user2 := User{Name: "Eric", Age: 20}
 	user3 := User{Name: "George", Age: 30}
 	user4 := User{Name: "Ringo", Age: 40}
+
+	users := Users{}
+	users = append(users, &user1)
+	users = append(users, &user2)
+	users = append(users, &user3, &user4)
+	fmt.Println(users)
 }
