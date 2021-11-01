f51ffeeabfd6beeda6e29c7ba4a8ed1264dc5ae7
Liberbell <liberbell@gmail.com>
2021-11-01 20:21:25 +0900

    main8

--- pj49/main.go
+++ pj49/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"fmt"
 	. "fmt"
 	f "fmt"
 	"foo"
 )
+
+func appName() string {
+	const AppName = "GOApp"
+	var Version string = "1.0"
+	return AppName + " " + Version
+}
 
 func main() {
 	f.Println(foo.Max)
 	// fmt.Println(foo.min)
 	f.Println(foo.ReturnMin())
 	Println(foo.Max)
+
+	fmt.Println(appName())
 }
