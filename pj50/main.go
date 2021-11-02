b8c59c3b983f3c26f8280d6ef42d53d5ba0e546f
Liberbell <liberbell@gmail.com>
2021-11-02 09:05:15 +0900

    main1

--- pj50/main.go
+++ pj50/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"alib"
+	"fmt"
+)
 
 func IsONe(i int) bool {
 	if i == 1 {
@@ -13,4 +16,7 @@ func IsONe(i int) bool {
 func main() {
 	fmt.Println(IsONe(1))
 	fmt.Println(IsONe(0))
+
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(alib.Average(s))
 }
