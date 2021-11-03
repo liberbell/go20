a60bcdad43606f2b3e7afa9fdae7d4e8af65fb5c
Liberbell <liberbell@gmail.com>
2021-11-04 08:45:24 +0900

    main3

--- pj53/main.go
+++ pj53/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"time"
 )
 
 func main() {
-	f, err := os.OpenFile("foo.txt", os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer f.Close()
+	// f, err := os.OpenFile("foo.txt", os.O_RDWR|os.O_CREATE, 0666)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// defer f.Close()
 
-	bs := make([]byte, 128)
-	n, err := f.Read(bs)
-	fmt.Println(n)
-	fmt.Println(string(bs))
+	// bs := make([]byte, 128)
+	// n, err := f.Read(bs)
+	// fmt.Println(n)
+	// fmt.Println(string(bs))
+
+	t := time.Now()
+	fmt.Println(t)
 }
