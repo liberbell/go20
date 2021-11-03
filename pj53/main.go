0285fbaaf74cfd91e2eb77973dd74f804293fa77
Liberbell <liberbell@gmail.com>
2021-11-04 08:41:49 +0900

    main2

--- pj53/main.go
+++ pj53/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -10,4 +11,10 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer f.Close()
+
+	bs := make([]byte, 128)
+	n, err := f.Read(bs)
+	fmt.Println(n)
+	fmt.Println(string(bs))
 }
