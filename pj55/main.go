df7a1d3675b3523c12b01ea321a8fa3aae1e919c
Liberbell <liberbell@gmail.com>
2021-11-05 08:53:19 +0900

    main8

--- pj55/main.go
+++ pj55/main.go
@@ -17,15 +17,17 @@ func main() {
 
 	// fmt.Println(math.MaxInt64)
 	// fmt.Println(math.MinInt32)
-	fmt.Println(math.Abs(100))
-	fmt.Println(math.Abs(-100))
+	// fmt.Println(math.Abs(100))
+	// fmt.Println(math.Abs(-100))
 
-	fmt.Println(math.Pow(0, 2))
-	fmt.Println(math.Pow(2, 2))
+	// fmt.Println(math.Pow(0, 2))
+	// fmt.Println(math.Pow(2, 2))
 
-	fmt.Println(math.Sqrt(2))
-	fmt.Println(math.Cbrt(8))
+	// fmt.Println(math.Sqrt(2))
+	// fmt.Println(math.Cbrt(8))
 
-	fmt.Println(math.Max(1, 2))
-	fmt.Println(math.Min(1, 2))
+	// fmt.Println(math.Max(1, 2))
+	// fmt.Println(math.Min(1, 2))
+	fmt.Println(math.Trunc(3.14))
+	fmt.Println(math.Trunc(-3.14))
 }
