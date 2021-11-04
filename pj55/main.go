992e0f88152154abfb538576046cbda2c03ebd89
Liberbell <liberbell@gmail.com>
2021-11-05 08:48:24 +0900

    main5

--- pj55/main.go
+++ pj55/main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
-	fmt.Println(math.Pi)
+	// fmt.Println(math.Pi)
 
-	fmt.Println(math.Sqrt2)
-	fmt.Println(math.MaxFloat32)
-	fmt.Println(math.SmallestNonzeroFloat32)
+	// fmt.Println(math.Sqrt2)
+	// fmt.Println(math.MaxFloat32)
+	// fmt.Println(math.SmallestNonzeroFloat32)
 
-	fmt.Println(math.MaxFloat64)
-	fmt.Println(math.SmallestNonzeroFloat64)
+	// fmt.Println(math.MaxFloat64)
+	// fmt.Println(math.SmallestNonzeroFloat64)
 
-	fmt.Println(math.MaxInt64)
-	fmt.Println(math.MinInt32)
+	// fmt.Println(math.MaxInt64)
+	// fmt.Println(math.MinInt32)
+	fmt.Println(math.Abs(100))
+	fmt.Println(math.Abs(-100))
 }
