211413f75c7368e5a70ff43060a3257f449499fa
Liberbell <liberbell@gmail.com>
2021-11-04 09:00:31 +0900

    main2

--- pj54/main.go
+++ pj54/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	t5 := time.Date(2021, 11, 4, 10, 10, 10, 0, time.Local)
 	t6 := time.Now()
+	fmt.Println(t6)
+
 	d2 := t5.Sub(t6)
+
 }
