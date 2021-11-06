2ed3b12784de09d7d9ba370ec305ce9c65ac99be
Liberbell <liberbell@gmail.com>
2021-11-06 17:07:33 +0900

    main4

--- pj59/main.go
+++ pj59/main.go
@@ -8,7 +8,11 @@ import (
 func main() {
 	log.SetOutput(os.Stdout)
 
-	log.Print("Log\n")
-	log.Printf("Log%d\n", 3)
-	log.Println("Log2")
+	// log.Print("Log\n")
+	// log.Printf("Log%d\n", 3)
+	// log.Println("Log2")
+
+	log.Fatal()
+	log.Fatalln()
+	log.Fatalf()
 }
