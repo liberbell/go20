595096687c0df37daf1ea795dd7164598541ec10
Liberbell <liberbell@gmail.com>
2021-11-07 10:26:16 +0900

    main3

--- pj61/main.go
+++ pj61/main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-	bt := true
-	fmt.Printf("%T\n", strconv.FormatBool(bt))
+	// bt := true
+	// fmt.Printf("%T\n", strconv.FormatBool(bt))
 
-	i := strconv.FormatInt(-100, 10)
-	fmt.Printf("%v, %T\n", i, i)
+	// i := strconv.FormatInt(-100, 10)
+	// fmt.Printf("%v, %T\n", i, i)
 
-	i2 := strconv.Itoa(100)
-	fmt.Printf("%v, %T\n", i2, i2)
+	// i2 := strconv.Itoa(100)
+	// fmt.Printf("%v, %T\n", i2, i2)
+
+	fmt.Println(strconv.FormatFloat(123.456, 'E', -1, 64))
+	fmt.Println(strconv.FormatFloat(123.456, 'e', 2, 64))
 }
