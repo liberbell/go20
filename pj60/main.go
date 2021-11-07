ad25256b98acaae41c9169caffc1e8d3804e60e2
Liberbell <liberbell@gmail.com>
2021-11-07 10:14:37 +0900

    main4

--- pj60/main.go
+++ pj60/main.go
@@ -6,18 +6,22 @@ import (
 )
 
 func main() {
-	log.SetOutput(os.Stdout)
+	// log.SetOutput(os.Stdout)
 
-	log.SetFlags(log.LstdFlags)
-	log.Println("A")
+	// log.SetFlags(log.LstdFlags)
+	// log.Println("A")
 
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
-	log.Println("B")
+	// log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	// log.Println("B")
 
-	log.SetFlags(log.Ltime | log.Lshortfile)
-	log.Println("C")
+	// log.SetFlags(log.Ltime | log.Lshortfile)
+	// log.Println("C")
 
-	log.SetFlags(log.LstdFlags)
-	log.SetPrefix("[LOG]")
-	log.Println("E")
+	// log.SetFlags(log.LstdFlags)
+	// log.SetPrefix("[LOG]")
+	// log.Println("E")
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	logger.Println("message")
+	log.Println("message")
 }
