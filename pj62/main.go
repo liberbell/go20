b3e202bedc98bab36a0bd3bcf2b5321c4fd356bb
Liberbell <liberbell@gmail.com>
2021-11-08 08:54:26 +0900

    main2

--- pj62/main.go
+++ pj62/main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	s1 := strings.Join([]string{"A", "B", "C"}, ",")
-	s2 := strings.Join([]string{"A", "B", "C"}, "")
-	fmt.Println(s1)
-	fmt.Println(s2)
+	// s1 := strings.Join([]string{"A", "B", "C"}, ",")
+	// s2 := strings.Join([]string{"A", "B", "C"}, "")
+	// fmt.Println(s1)
+	// fmt.Println(s2)
+
+	i1 := strings.LastIndex("ABCDEFGHI", "BC")
+	fmt.Println(i1)
 }
