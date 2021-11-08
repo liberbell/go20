2250c372a685ab856b90199c9d1775231a822a70
Liberbell <liberbell@gmail.com>
2021-11-08 09:05:42 +0900

    main9

--- pj62/main.go
+++ pj62/main.go
@@ -11,27 +11,31 @@ func main() {
 	// fmt.Println(s1)
 	// fmt.Println(s2)
 
-	i1 := strings.Index("ABCDEFGHI", "A")
-	i2 := strings.Index("ABCDEFGHI", "D")
-	fmt.Println(i1, i2)
+	// i1 := strings.Index("ABCDEFGHI", "A")
+	// i2 := strings.Index("ABCDEFGHI", "D")
+	// fmt.Println(i1, i2)
 
-	i3 := strings.LastIndex("ABCDABCD", "BC")
-	fmt.Println(i3)
+	// i3 := strings.LastIndex("ABCDABCD", "BC")
+	// fmt.Println(i3)
 
-	i4 := strings.IndexAny("ABC", "ABC")
+	// i4 := strings.IndexAny("ABC", "ABC")
 
-	i5 := strings.LastIndexAny("ABC", "ABC")
-	fmt.Println(i4, i5)
+	// i5 := strings.LastIndexAny("ABC", "ABC")
+	// fmt.Println(i4, i5)
 
-	b1 := strings.HasPrefix("ABC", "A")
-	b2 := strings.HasSuffix("ABC", "C")
-	fmt.Println(b1, b2)
+	// b1 := strings.HasPrefix("ABC", "A")
+	// b2 := strings.HasSuffix("ABC", "C")
+	// fmt.Println(b1, b2)
 
-	b3 := strings.Contains("ABC", "B")
-	b4 := strings.ContainsAny("ABCDE", "BD")
-	fmt.Println(b3, b4)
+	// b3 := strings.Contains("ABC", "B")
+	// b4 := strings.ContainsAny("ABCDE", "BD")
+	// fmt.Println(b3, b4)
 
-	i6 := strings.Count("ABCABC", "B")
-	i7 := strings.Count("ABCABC", "")
-	fmt.Println(i6, i7)
+	// i6 := strings.Count("ABCABC", "B")
+	// i7 := strings.Count("ABCABC", "")
+	// fmt.Println(i6, i7)
+
+	s3 := strings.Repeat("ABC", 4)
+	s4 := strings.Repeat("ABC", 0)
+	fmt.Println(s3, s4)
 }
