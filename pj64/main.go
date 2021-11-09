82c4c27f74175c73985790ca8077a51640cadba6
Liberbell <liberbell@gmail.com>
2021-11-10 08:44:11 +0900

    main3

--- pj64/main.go
+++ pj64/main.go
@@ -6,24 +6,24 @@ import (
 )
 
 func main() {
-	match, _ := regexp.MatchString("A", "ABC")
-	fmt.Println(match)
+	// match, _ := regexp.MatchString("A", "ABC")
+	// fmt.Println(match)
 
-	re1, _ := regexp.Compile("A")
-	match = re1.MatchString("ABC")
-	fmt.Println(match)
+	// re1, _ := regexp.Compile("A")
+	// match = re1.MatchString("ABC")
+	// fmt.Println(match)
 
-	re2 := regexp.MustCompile("A")
-	match = re2.MatchString("ABC")
-	fmt.Println(match)
+	// re2 := regexp.MustCompile("A")
+	// match = re2.MatchString("ABC")
+	// fmt.Println(match)
 
 	// i 大文字小文字を意識しない
 	// m マルチラインモード
 	// s .が\nにマッチ
 	// U 最小マッチへの変換　x*はx*?、x+はx+?に
-	re3 := regexp.MustCompile(`(?i)abc`)
-	match = re3.MatchString("ABC")
-	fmt.Println(match)
+	// re3 := regexp.MustCompile(`(?i)abc`)
+	// match = re3.MatchString("ABC")
+	// fmt.Println(match)
 
 	// ^ 文頭(mフラグの場合行末も)
 	// $ 文末(mフラグの場合行頭も)
