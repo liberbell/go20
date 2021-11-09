3deac395eb15dc77c1db779aeb40c58b76eec483
Liberbell <liberbell@gmail.com>
2021-11-10 08:47:17 +0900

    main5

--- pj64/main.go
+++ pj64/main.go
@@ -39,6 +39,17 @@ func main() {
 	// match = re4.MatchString("   ABC   ")
 	// fmt.Println(match)
 
+	x+ 0
+	x* 1
+	x? 0
+	x{n, m} n
+	x{n, } n
+	x{n} n
+	x*? 0
+	x+? 1
+	x?? 0
+	
+
 	re5 := regexp.MustCompile("a+b*")
 	fmt.Println(re5.MatchString("ab"))
 	fmt.Println(re5.MatchString("a"))
