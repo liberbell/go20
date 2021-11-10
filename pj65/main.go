00ecc0eff6b030a47723dad1e37d45bb7d59ac93
Liberbell <liberbell@gmail.com>
2021-11-10 09:06:57 +0900

    main4

--- pj65/main.go
+++ pj65/main.go
@@ -12,4 +12,11 @@ func main() {
 	fmt.Println(fs1)
 	fs2 := re.FindAllString("ABCXYZABCXYZ", -1)
 	fmt.Println(fs2)
+
+	re1 := regexp.MustCompile(`(\d+)-(\d+)-(\d)`)
+	s := `
+		0120-123-456
+		111-222-333
+		090-1234-5678
+		`
 }
