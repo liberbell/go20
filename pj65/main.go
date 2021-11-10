73e67692d2fd2ecb627dfb56d33fb872f3579022
Liberbell <liberbell@gmail.com>
2021-11-11 08:53:34 +0900

    main6

--- pj65/main.go
+++ pj65/main.go
@@ -13,21 +13,24 @@ func main() {
 	// fs2 := re.FindAllString("ABCXYZABCXYZ", -1)
 	// fmt.Println(fs2)
 
-	re1 := regexp.MustCompile(`(\d+)-(\d+)-(\d+)`)
-	s := `
-		0120-456-7899
-		111-222-333
-		090-1234-5678
-		`
+	// re1 := regexp.MustCompile(`(\d+)-(\d+)-(\d+)`)
+	// s := `
+	// 	0120-456-7899
+	// 	111-222-333
+	// 	090-1234-5678
+	// 	`
 
-	ms := re1.FindAllStringSubmatch(s, -1)
-	fmt.Println(ms)
+	// ms := re1.FindAllStringSubmatch(s, -1)
+	// fmt.Println(ms)
 
-	for _, v := range ms {
-		fmt.Println(v)
-	}
+	// for _, v := range ms {
+	// 	fmt.Println(v)
+	// }
 
-	fmt.Println(ms[0][0])
-	fmt.Println(ms[0][1])
-	fmt.Println(ms[0][2])
+	// fmt.Println(ms[0][0])
+	// fmt.Println(ms[0][1])
+	// fmt.Println(ms[0][2])
+
+	re2 := regexp.MustCompile(`\s+`)
+	fmt.Println(re2.ReplaceAllString("AAA BBB CCC", ","))
 }
