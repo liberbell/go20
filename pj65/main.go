152be4e66b4504232b7eed2a4b5a22262ba1f793
Liberbell <liberbell@gmail.com>
2021-11-11 08:45:57 +0900

    main2

--- pj65/main.go
+++ pj65/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
-	re := regexp.MustCompile(`(abc|ABC)(xyz|XYZ)`)
+	// re := regexp.MustCompile(`(abc|ABC)(xyz|XYZ)`)
 
-	fs1 := re.FindString("AAAAABCXYZZZZZZ")
-	fmt.Println(fs1)
-	fs2 := re.FindAllString("ABCXYZABCXYZ", -1)
-	fmt.Println(fs2)
+	// fs1 := re.FindString("AAAAABCXYZZZZZZ")
+	// fmt.Println(fs1)
+	// fs2 := re.FindAllString("ABCXYZABCXYZ", -1)
+	// fmt.Println(fs2)
 
 	re1 := regexp.MustCompile(`(\d+)-(\d+)-(\d)`)
 	s := `
-		0120-123-456
+		0120-456-7899
 		111-222-333
 		090-1234-5678
 		`
@@ -27,5 +27,5 @@ func main() {
 		fmt.Println(v)
 	}
 
-	fmt.Println(ms[0][0])
+	// fmt.Println(ms[0][0])
 }
