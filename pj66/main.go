cf6b0ccbb0ac120f4ee50411be0c50956208c5d6
Liberbell <liberbell@gmail.com>
2021-11-11 09:07:48 +0900

    main3

--- pj66/main.go
+++ pj66/main.go
@@ -1,11 +1,18 @@
 package main
 
+import "time"
+
 var st struct {
 	A, B, C int
 }
 
 func UpdateAndPrint(n int) {
 	st.A = n
+	time.Sleep(time.Microsecond)
+	st.B = n
+	time.Sleep(time.Microsecond)
+	st.C = n
+	time.Sleep(time.Microsecond)
 }
 
 func main() {
