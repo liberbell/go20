bf65f8f61cfc835eccaf700712aedf6e148e388c
Liberbell <liberbell@gmail.com>
2021-11-12 08:45:15 +0900

    main2

--- pj67/main.go
+++ pj67/main.go
@@ -8,4 +8,16 @@ func main() {
 			fmt.Println("1st Go routing.")
 		}
 	}()
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("2nd Go routing.")
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("3rd Go routing.")
+		}
+	}()
 }
