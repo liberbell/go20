f48ba115b6767e8db26dd8cf137abd8ec455ef01
Liberbell <liberbell@gmail.com>
2021-11-12 08:48:27 +0900

    main4

--- pj67/main.go
+++ pj67/main.go
@@ -1,27 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+
+	wg := new(sync.WaitGroup)
+	wg.Add(3)
+
 	go func() {
 		for i := 0; i < 100; i++ {
 			fmt.Println("1st Go routing.")
 		}
+		wg.Done()
 	}()
 
 	go func() {
 		for i := 0; i < 100; i++ {
 			fmt.Println("2nd Go routing.")
 		}
+		wg.Done()
 	}()
 
 	go func() {
 		for i := 0; i < 100; i++ {
 			fmt.Println("3rd Go routing.")
 		}
+		wg.Done()
 	}()
 
-	for {
+	wg.Wait()
 
-	}
+	// for {
+
+	// }
 }
