f93e478e16c2ebaf0dbae2bebdc3eef40e412953
Liberbell <liberbell@gmail.com>
2021-11-12 08:44:47 +0900

    main2

--- pj67/main.go
+++ pj67/main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	go func ()  {
-		for i := 0; i < 100 ; i++ {
+	go func() {
+		for i := 0; i < 100; i++ {
 			fmt.Println("1st Go routing.")
 		}
-	}
+	}()
 }
