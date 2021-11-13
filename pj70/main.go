93ccaf22cc0413d32149eaeebab4e57b2045e783
Liberbell <liberbell@gmail.com>
2021-11-13 10:28:04 +0900

    main2

--- pj70/main.go
+++ pj70/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	i := []int{5, 3, 2, 4, 8, 9, 1, 0, 11, 7}
 	s := []string{"a", "z", "h", "k"}
 
+	fmt.Println(i, s)
+
+	sort.Ints(i)
+	sort.Strings(s)
 	fmt.Println(i, s)
 }
