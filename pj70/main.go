4cecd25d9fb3d72473bfed402d1991ce81ebfa93
Liberbell <liberbell@gmail.com>
2021-11-13 10:30:22 +0900

    main3

--- pj70/main.go
+++ pj70/main.go
@@ -1,17 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+type Entry struct {
+	Name  string
+	value int
+}
 
 func main() {
-	i := []int{5, 3, 2, 4, 8, 9, 1, 0, 11, 7}
-	s := []string{"a", "z", "h", "k"}
+	// i := []int{5, 3, 2, 4, 8, 9, 1, 0, 11, 7}
+	// s := []string{"a", "z", "h", "k"}
 
-	fmt.Println(i, s)
+	// fmt.Println(i, s)
 
-	sort.Ints(i)
-	sort.Strings(s)
-	fmt.Println(i, s)
+	// sort.Ints(i)
+	// sort.Strings(s)
+	// fmt.Println(i, s)
 }
