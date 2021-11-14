45f478884e6395b727b6d404e8b133c48cfc33c7
Liberbell <liberbell@gmail.com>
2021-11-15 08:55:18 +0900

    main7

--- pj74/main.go
+++ pj74/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"net/url"
 )
 
 func main() {
-	res, _ := http.Get("https://example.com")
-	fmt.Println(res.StatusCode)
-	fmt.Println(res.Proto)
-	fmt.Println(res.Header["Date"])
-	fmt.Println(res.Header["Content-Type"])
+	// res, _ := http.Get("https://example.com")
+	// fmt.Println(res.StatusCode)
+	// fmt.Println(res.Proto)
+	// fmt.Println(res.Header["Date"])
+	// fmt.Println(res.Header["Content-Type"])
 
-	fmt.Println(res.Request.Method)
-	fmt.Println(res.Request.URL)
+	// fmt.Println(res.Request.Method)
+	// fmt.Println(res.Request.URL)
 
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(body))
+	// defer res.Body.Close()
+	// body, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(body))
 
 	vs := url.Values{}
 	vs.Add("id", "1")
