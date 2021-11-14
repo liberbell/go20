f74d89c20e9a2268de554abd4cc47701aceeb4a0
Liberbell <liberbell@gmail.com>
2021-11-14 13:48:26 +0900

    main9

--- pj71/main.go
+++ pj71/main.go
@@ -16,6 +16,10 @@ func (l List) Len() int {
 	return len(l)
 }
 
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
 func (l List) Less(i, j int) bool {
 	if l[i].Value == l[j].Value {
 		return (l[i].Name < l[j].Name)
