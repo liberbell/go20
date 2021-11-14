95eecb5cf06b528e1193c3dcfa80a1c85822c9c1
Liberbell <liberbell@gmail.com>
2021-11-14 10:58:56 +0900

    main2

--- pj71/main.go
+++ pj71/main.go
@@ -1,5 +1,15 @@
 package main
 
+type Entry struct {
+	Name string
+	Value int
+}
+
+type List []Entry
+
 func main() {
 	m := map[string]int{"S": 1, "J": 4, "A": 3, "N": 3}
+	lt := List{}
+	for, k, v := 
+
 }
