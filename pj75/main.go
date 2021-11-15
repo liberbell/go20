78bfe81aa93598897c8bd3962acdddb3836b0068
Liberbell <liberbell@gmail.com>
2021-11-15 09:10:00 +0900

    main8

--- pj75/main.go
+++ pj75/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -11,13 +13,13 @@ func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World.")
 }
 
-// func top(w http.ResponseWriter, r *http.Request) {
-// 	t, err := template.ParseFiles("tepl.html")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	t.Execute(w, "Hello Go World.")
-// }
+func top(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("tepl.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, "Hello Go World.")
+}
 
 func main() {
 	http.ListenAndServe(":8000", &MyHandler{})
