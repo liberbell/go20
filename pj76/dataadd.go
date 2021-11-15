d62efdb9bb04be0de8f310a23b6bb09e057d3524
Liberbell <liberbell@gmail.com>
2021-11-16 08:58:29 +0900

    dataadd2

--- pj76/dataadd.go
+++ pj76/dataadd.go
@@ -1,9 +1,19 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 var Db *sql.DB
 
 func main() {
 	Db, _ := sql.Open("sqlite3", "./exampl.sql")
+	defer Db.Close()
+
+	cmd := `INSERT INTO persons (name, age) values (7, 7)`
+	_, err := Db.Exec(cmd, "Bob", 47)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
