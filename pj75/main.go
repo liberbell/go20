1845b5ca12918e35c8d3c096ed4915f7d7cc66bf
Liberbell <liberbell@gmail.com>
2021-11-15 09:03:53 +0900

    main2

--- pj75/main.go
+++ pj75/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type MyHandler struct{}
 
-func (h *MyHandler) ServeHTTP() {
-
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello World.")
 }
 
 func main() {
