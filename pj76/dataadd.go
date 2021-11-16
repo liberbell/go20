54f226fa2b607b1bc70ec5f643652ade41edd8fa
Liberbell <liberbell@gmail.com>
2021-11-16 09:03:29 +0900

    dataadd6

--- pj76/dataadd.go
+++ pj76/dataadd.go
@@ -13,7 +13,7 @@ func main() {
 	Db, _ := sql.Open("sqlite3", "./exampl.sql")
 	defer Db.Close()
 
-	cmd := "INSERT INTO persons (name, age) values (?, ?)"
+	cmd := "INSERT INTO persons (name, age) VALUES (?, ?)"
 	_, err := Db.Exec(cmd, "Bob", 47)
 	if err != nil {
 		log.Fatal(err)
