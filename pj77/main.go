5b21de7f2134da8c5915c34903622d5630752fc3
Liberbell <liberbell@gmail.com>
2021-11-18 08:54:18 +0900

    main6

--- pj77/main.go
+++ pj77/main.go
@@ -18,7 +18,7 @@ type Person struct {
 func main() {
 	Db, err := sql.Open("postgres", "user=test_user1 dbname=testdb1 password=user1pass sslmode=disable")
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 	defer Db.Close()
 
