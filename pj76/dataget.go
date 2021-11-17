d612ff75e7341764696a3b23188849d1a0e27300
Liberbell <liberbell@gmail.com>
2021-11-17 12:09:10 +0900

    dataget3

--- pj76/dataget.go
+++ pj76/dataget.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -19,17 +17,22 @@ func main() {
 	Db, _ := sql.Open("sqlite3", "./example.sql")
 	defer Db.Close()
 
-	cmd := "SELECT * FROM persons WHERE age = ?"
-	row := Db.QueryRow(cmd, 55)
-	var p Person
-	err := row.Scan(&p.Name, &p.Age)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			fmt.Println("No Row")
-		} else {
-			log.Println(err)
-		}
-	}
-	fmt.Println(p.Name, p.Age)
+	// cmd := "SELECT * FROM persons WHERE age = ?"
+	// row := Db.QueryRow(cmd, 55)
+	// var p Person
+	// err := row.Scan(&p.Name, &p.Age)
+	// if err != nil {
+	// 	if err == sql.ErrNoRows {
+	// 		fmt.Println("No Row")
+	// 	} else {
+	// 		log.Println(err)
+	// 	}
+	// }
+	// fmt.Println(p.Name, p.Age)
+
+	cmd := "SELECT * FROM persons"
+
+	rows, _ := Db.Query(cmd)
+	defer rows.Close()
 
 }
