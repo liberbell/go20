9b57aa566fcbf306c0c5df6a1c2269d545a4fbf1
Liberbell <liberbell@gmail.com>
2021-11-18 08:52:51 +0900

    main5

--- pj77/main.go
+++ pj77/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
@@ -15,7 +16,10 @@ type Person struct {
 }
 
 func main() {
-	Db, _ := sql.Open("postgres", "user=test_user1 dbname=testdb1 password=user1pass sslmode=disable")
+	Db, err := sql.Open("postgres", "user=test_user1 dbname=testdb1 password=user1pass sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer Db.Close()
 
 	fmt.Println("a")
