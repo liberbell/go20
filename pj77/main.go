b11fca7ebcdbb96b3dd2eb6628da86e68596b417
Liberbell <liberbell@gmail.com>
2021-11-18 08:49:48 +0900

    main1

--- pj77/main.go
+++ pj77/main.go
@@ -2,6 +2,13 @@ package main
 
 import "database/sql"
 
+var Db *sql.DB
+
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	Db, _ := sql.Open("postgres", "user= dbname=test_db1 password=")
 	defer Db.Close()
