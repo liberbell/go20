16b768b9ab58b5f1b7778a08f9c15db2612b061b
Liberbell <liberbell@gmail.com>
2021-11-17 12:16:55 +0900

    datadel2

--- pj76/datadel.go
+++ pj76/datadel.go
@@ -1,8 +1,21 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var Db *sql.DB
 
 func main() {
 	Db, _ := sql.Open("sqlite3", "./example.sql")
 	defer Db.Close()
+
+	cmd := "DELETE FROM persons WHERE name = ?"
+	_, err := Db.Exec(cmd, "Bob")
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
