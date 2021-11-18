8afde536efa298e891c982733176d7f3025a23a9
Liberbell <liberbell@gmail.com>
2021-11-19 08:56:45 +0900

    main3

--- pj78/main.go
+++ pj78/main.go
@@ -12,6 +12,11 @@ var Config ConfigList
 
 func init() {
 	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustInt(8080),
+		DbName:    cfg.Section("db").Key("name").MustString("example.sql"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+	}
 }
 
 func main() {
