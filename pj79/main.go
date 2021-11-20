79bee4efadb3c6c4539d33d42e7d47c8da961658
Liberbell <liberbell@gmail.com>
2021-11-20 14:35:00 +0900

    main4

--- pj79/main.go
+++ pj79/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"pj79/apps/models"
+)
+
 func main() {
 	// fmt.Println(config.Config.Port)
 	// fmt.Println(config.Config.SQLDriver)
@@ -7,5 +12,7 @@ func main() {
 	// fmt.Println(config.Config.LogFile)
 
 	// log.Println("test")
+
+	fmt.Println(models.Db)
 
 }
