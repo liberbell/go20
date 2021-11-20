a81ad90ddd01fe55e9efd7206f4e1989f3fe222b
Liberbell <liberbell@gmail.com>
2021-11-20 14:01:48 +0900

    main1

---
+++ pj79/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mgechev/revive/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
