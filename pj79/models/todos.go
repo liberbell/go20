0b2bb295ae2b3d9acb11549415c0e84e258d8356
Liberbell <liberbell@gmail.com>
2021-11-22 08:49:43 +0900

    todos2

--- pj79/models/todos.go
+++ pj79/models/todos.go
@@ -1,10 +1,27 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 type Todo struct {
 	ID         int
 	Content    string
 	UserID     int
 	created_at time.Time
+}
+
+func (u *User) CreateTodos(content string) (err error) {
+	cmd := fmt.Sprintf(`INSERT INTO todos (
+		content,
+		user_id,
+		created_at) VALUES (?, ?, ?)`)
+
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
 }
