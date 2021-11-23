3c4c9b9da812822bcce6ffd6ff8c705ebe94809d
Liberbell <liberbell@gmail.com>
2021-11-23 16:47:47 +0900

    todos6

--- pj79/apps/models/todos.go
+++ pj79/apps/models/todos.go
@@ -63,6 +63,21 @@ func GetTodos() (todos []Todo, err error) {
 	return todos, err
 }
 
-func (u *User) GetTodosByUser() (todos []Todo) {
+func (u *User) GetTodosByUser() (todos []Todo, err error) {
+	cmd := `SELECT id, content, user_id, created_at FROM todos WHERE user_id = ?`
+	rows, err := Db.Query(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for rows.Next() {
+		var todo Todo
+		err = rows.Scan(&todo.ID,
+			&todo.Content,
+			&todo.UserID,
+			&todo.created_at)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
+	}
 }
