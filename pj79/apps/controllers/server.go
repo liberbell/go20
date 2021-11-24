c65246bf5bba58bf2f8bf21e65dcba74aac27f1d
Liberbell <liberbell@gmail.com>
2021-11-25 08:58:30 +0900

    server1

--- pj79/apps/controllers/server.go
+++ pj79/apps/controllers/server.go
@@ -1,9 +1,17 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"pj79/config"
 )
+
+func GenerateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("apps/views/templates/%s.html", file))
+	}
+}
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
