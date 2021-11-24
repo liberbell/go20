60a133d5fea2df1f25f2ea759796fed558087e62
Liberbell <liberbell@gmail.com>
2021-11-25 08:35:36 +0900

    server1

--- pj79/apps/controllers/server.go
+++ pj79/apps/controllers/server.go
@@ -7,6 +7,8 @@ import (
 
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
+	http.HandleFunc("/static/", http.StripPrefix("/static/", files))
+
 	http.HandleFunc("/", top)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
