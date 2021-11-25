4f9296ed8ff3f5c150065112dcf43e319989dafb
Liberbell <liberbell@gmail.com>
2021-11-26 08:47:24 +0900

    routeauth2

--- pj79/apps/controllers/route_auth.go
+++ pj79/apps/controllers/route_auth.go
@@ -3,5 +3,5 @@ package controllers
 import "net/http"
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "signup")
+	GenerateHTML(w, nil, "layout", "signup")
 }
