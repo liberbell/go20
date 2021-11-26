057a53a73b0a399734d687070e5b51fd90e09815
Liberbell <liberbell@gmail.com>
2021-11-26 09:07:42 +0900

    routeauth3

--- pj79/apps/controllers/route_auth.go
+++ pj79/apps/controllers/route_auth.go
@@ -1,7 +1,23 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"pj79/apps/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	GenerateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		GenerateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			Name: r.PostFormValue("name"),
+			Email: r.PostFormValue("email")
+		}
+	}
+
 }
