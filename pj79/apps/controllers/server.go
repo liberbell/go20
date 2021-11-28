426eb6c8131d468a92636866359afa4cfd4a8f2d
Liberbell <liberbell@gmail.com>
2021-11-29 08:49:41 +0900

    server

--- pj79/apps/controllers/server.go
+++ pj79/apps/controllers/server.go
@@ -27,6 +27,7 @@ func StartMainServer() error {
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/index", index)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
 
