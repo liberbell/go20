fffd35862ff2bcf6776a2b4d91f8d0dea2bafb33
Liberbell <liberbell@gmail.com>
2021-12-03 08:58:50 +0900

    server3

--- pj79/apps/controllers/server.go
+++ pj79/apps/controllers/server.go
@@ -35,6 +35,7 @@ func StartMainServer() error {
 	http.HandleFunc("/todos/save", todoSave)
 	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
 	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
+	http.HandleFunc("/todos/delete/", parseURL(todoDelete))
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
 
