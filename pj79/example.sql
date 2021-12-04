d7087f2ea422ed30db891b09e471ec28f88dccf7
Liberbell <liberbell@gmail.com>
2021-12-04 14:50:07 +0900

    example1

---
+++ pj79/example.sql
@@ -0,0 +1,12 @@
+drop table sessions;
+drop table users;
+drop table todos;
+
+create table users (
+    id serial primary key,
+    uuid varchar(64) not null unique,
+    name varchar(255),
+    email varchar(255) not null unique,
+    password varchar(255) not null,
+    created_at timestamp not null
+);
