e234b1a9ad2b70d0b856a6fb399c47f5cd3b7b72
Liberbell <liberbell@gmail.com>
2021-12-04 14:43:22 +0900

    base1

--- pj79/apps/models/base.go
+++ pj79/apps/models/base.go
@@ -4,12 +4,9 @@ import (
 	"crypto/sha1"
 	"database/sql"
 	"fmt"
-	"log"
-
-	"pj79/config"
 
 	"github.com/google/uuid"
-	_ "github.com/mattn/go-sqlite3"
+	// _ "github.com/mattn/go-sqlite3"
 )
 
 var Db *sql.DB
@@ -23,36 +20,36 @@ const (
 )
 
 func init() {
-	Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		uuid STRING NOT NULL UNIQUE,
-		name STRING,
-		email STRING,
-		password STRING,
-		created_at DATETIME)`, tableNameUser)
+	// Db, err = sql.Open(config.Config.SQLDriver, config.Config.DbName)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+	// 	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	// 	uuid STRING NOT NULL UNIQUE,
+	// 	name STRING,
+	// 	email STRING,
+	// 	password STRING,
+	// 	created_at DATETIME)`, tableNameUser)
 
-	Db.Exec(cmdU)
+	// Db.Exec(cmdU)
 
-	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		content TEXT,
-		user_id INTEGER,
-		created_at DATETIME)`, tableNameTodo)
+	// cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+	// 	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	// 	content TEXT,
+	// 	user_id INTEGER,
+	// 	created_at DATETIME)`, tableNameTodo)
 
-	Db.Exec(cmdT)
+	// Db.Exec(cmdT)
 
-	cmdS := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		uuid STRING NOT NULL UNIQUE,
-		email STRING,
-		user_id INTEGER,
-		created_at DATETIME)`, tableNameSession)
+	// cmdS := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+	// 	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	// 	uuid STRING NOT NULL UNIQUE,
+	// 	email STRING,
+	// 	user_id INTEGER,
+	// 	created_at DATETIME)`, tableNameSession)
 
-	Db.Exec(cmdS)
+	// Db.Exec(cmdS)
 
 }
 
